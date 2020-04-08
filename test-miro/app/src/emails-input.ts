2d18c09ecae1bc274462af830e04fa59970450b8
iamssa <alexandrsimakovv@mail.ru>
2020-04-08 15:29:31 +0500

    feat: added events in buttons and finalized email component
    - adding random email button
    - getting valid email count
    - comma check in input
    - change indexOf to regEx in validation
    - added "enter" processing

--- test-miro/app/src/emails-input.ts
+++ test-miro/app/src/emails-input.ts
@@ -3,7 +3,9 @@ const deteleEmailBox = (element: HTMLDivElement) => {
 }
 
 const isValidEmail = (email: string): boolean => {
-  return email.indexOf('@') !== -1;
+  const regEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  return email.match(regEx) !== null;
 }
 
 const showError = (emailBox: HTMLDivElement, title: HTMLDivElement) => {
@@ -35,12 +37,42 @@ const addEmailBox = (email: string) => {
       showError(emailBox, title);
     }
   };
+  title.onkeydown = (event) => {
+    if (event.code === 'Enter') {
+      event.preventDefault();
+      document.getElementById('emails-editor__input').focus();
+    }
+  }
 
   emailBox.append(title);
   emailBox.append(deleteBtn);
   emailsPanel[0].append(emailBox);
 
-  if (!isValidEmail(email)) {
-    showError(emailBox, title);
+  if (!isValidEmail(email)) showError(emailBox, title);
+}
+
+const checkCommaAndAddEmailBox = (emailInput: HTMLInputElement) => {
+  const inputValue = emailInput.value;
+  const comma = ',';
+
+  if (inputValue.indexOf(comma) === -1) return;
+
+  if (inputValue.indexOf(comma) === inputValue.length) {
+    addEmailBox(inputValue.slice(0, -1));
+    emailInput.value = null;
+  } else {
+    const indexComma = inputValue.indexOf(comma);
+    emailInput.value = emailInput.value.substring(indexComma + 1).trim();
+
+    addEmailBox(inputValue.slice(0, indexComma));
+    checkCommaAndAddEmailBox(emailInput);
   }
 }
+
+// export {
+//   deteleEmailBox,
+//   isValidEmail,
+//   showError,
+//   addEmailBox,
+//   checkCommaAndAddEmailBox
+// };
